3e5d61aab0dae03b50b2ce7e68a0a278bc0c029a
tindy360 <tindy360>
2017-04-20 07:21:56 -0400

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
+
+const Header = () => (
+<div>
+  <Button>Podcast Search</Button>
+  <Button>GitHub</Button>
+  <Button>Demo</Button>
+</div>
+)
+export default Header
